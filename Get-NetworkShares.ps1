6c87a441d0a462e2acadabe688b478f900778a5a
Simon Brown <simonb@efstechnology.com>
2018-08-31 13:54:18 +0100

    first commit

---
+++ Get-NetworkShares.ps1
@@ -0,0 +1,12 @@
+﻿Function Get-NetworkShares {
+Param (
+$server
+)
+	$result = net.exe view $server
+	$shares = @()
+	$result[7..($result.count-3)] | ForEach-Object {
+		$path = "\\" + $server + "\" + $_.Split(" ")[0] + "\"
+		$shares += @($path)
+	}
+	$shares
+}
